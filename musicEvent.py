b00beb64413aa47ac42ef96153ab77952d083927
cmolarti <cmolarti@gmail.com>
2023-12-30 19:31:43 -0500

    change method from using text files to using sql

--- musicEvent.py
+++ musicEvent.py
@@ -2,9 +2,15 @@ import requests
 import selectorlib as sl
 from send_message import send_msg
 import time
+import sqlite3 as sql
+
 
 
 URL = 'https://programmer100.pythonanywhere.com/tours/'
+
+
+info = sql.connect('data.db')
+
 
 def scrape(url):
     ''' Scrape the page source from url'''
@@ -21,7 +27,6 @@ def extract(source):
 
 def send_email(value):
     send_msg(value)
-    save_value(value)
 
 
 def save_value(value):
@@ -43,18 +48,60 @@ def open_file():
     return data
 
 
+def read_db(text_form):
+    global info
+
+    event = text_form.split(',')
+    event = [item.strip() for item in event]
+    band, city, date = event
+
+    cursor = info.cursor()
+    cursor.execute('SELECT * FROM events WHERE band=? AND city=? AND date=?', (band, city, date))
+    results = cursor.fetchall()
+
+    print(f'READ_DB: {results}')
+
+    return results
+
+
+def write_db(value):
+    global info
+
+    event = value.split(',')
+    event = [item.strip() for item in event]
+    print(f'event: {event}')
+    band, city, date = event
+
+    cursor = info.cursor()
+    cursor.execute('INSERT INTO events VALUES(?,?,?)', (band, city, date))
+    info.commit()
+
+    print(f'WRITE_DB...')
+
+
+
+
 if __name__ == '__main__':
     while True:
         text = scrape(URL)
         # print(text)
 
         value = extract(text)
-        # print(f'VALUES: {value}')
+        print(f'EXTRACTED: {value}')
 
-        data = open_file()
+        # data = open_file()        #using csv/text file
         # print(f'data: {data}')
-        if value != 'No upcoming tours' and value not in data:
-            send_email(value)
+        
+        if value != 'No upcoming tours':
+            results = read_db(value)
+            if len(results) == 0:
+                send_email(value)
+                # save_value(value) #using csv/text file
+
+                write_db(value)
+            else:
+                print(f'Event already exists')
+                
         
 
-        time.sleep(3600)
+        time.sleep(10)
